e8d9addc0790b1526531323bb8cecb47ba099135
Pablo Martinez <pablodmv@gmail.com>
2019-04-29 17:59:49 -0300

    Signup

--- src/lib/passport.js
+++ src/lib/passport.js
@@ -3,6 +3,28 @@ const localStrategy = require('passport-local').Strategy;
 const pool = require('../database');
 const  helper = require('../lib/helpers');
 
+passport.use('local.signin', new localStrategy({
+    usernameField: 'username',
+    passwordField: 'password',
+    passReqToCallback: true
+}, async (req,username, password, done)=>{
+
+    const rows = await pool.query('SELECT * FROM users WHERE username = ?', [username]);
+    if (rows.length > 0) {
+        const user = rows[0];
+        const validPassword = await helper.matchPassword(password, user.password);
+        if (validPassword) {
+            done(null, user, req.flash('success', 'Welcome' + user.username));
+        } else {
+            done(null, false, req.flash('message','Incorrect password'));
+        }
+    } else {
+        return done(null, false, req.flash('message', "nombre de usuario no existe"));
+    }
+}));
+
+
+
 passport.use('local.signup', new localStrategy({
     usernameField: 'username',
     passwordField: 'password',
